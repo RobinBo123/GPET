03d43ba8fffce5270b8dca1d2b61997e3e6e3d72
RobinBo123 <robin.bolz@h-ka.de>
2023-02-16 12:27:28 +0100

    Final commit

--- docs/index.js
+++ docs/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 // import Calculator from "./Calculator";
-import App from "./App";
+import App from "./docs/App";
 
 const root = ReactDOM.createRoot(document.getElementById('master'));
 root.render(<App />);
